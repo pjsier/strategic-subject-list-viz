494fbe1f60608817637e11d5eb6f88d2e55691b3
pjsier <pjsier@gmail.com>
2017-05-23 19:04:52 -0500

    cleaned up initial version of interactive treemap

--- js/ssl_treemap.js
+++ js/ssl_treemap.js
@@ -19,68 +19,29 @@ var fader = function(color) { return d3.interpolateRgb(color, "#fff")(0.2); },
 
 var treemap = d3.treemap()
     .tile(d3.treemapSquarify.ratio(1))
+    // .tile(d3.treemapResquarify)
     .size([width, height])
     .round(true)
     .paddingInner(1);
 
-
-function treemapForScore() {
-  var data = csvData;
-  var scoreData = data.filter(function(d) { return (d.ssl_score >= scoreNum) && (d.sex != "Other"); });
-  var scoreNest = d3.nest()
-    .key(function(d) { return d.race; })
-    .key(function(d) { return d.sex; })
-    .key(function(d) { return d.age; })
-    .rollup(function(d) { return d3.sum(d, function(d) { return +d.count; }); });
-
-  root = d3.hierarchy({values: scoreNest.entries(scoreData)}, function(d) { return d.values; })
-    .eachBefore(function(d) {
-      if (d.data.key) {
-        d.data.id = (d.parent ? d.parent.data.id + "." : "") + d.data.key.toLowerCase();
-      }
-      else {
-        d.data.id = "ssl";
-      }
-    })
-    .sum(function(d) { return d.value; })
-    .sort(function(a, b) { return b.height - a.height || b.value - a.value;; });
-
-  treemap = treemap.size([width, height]);
-  treemap(root);
-}
+// TODO:
+// - Figure out what is going on with resizing changing colors, label centers
+// - Add tooltips on both hover and click
+// - Logic for when overall segment labels should disappear
+// - Try to resize only when slider is let go
 
 
-function resize() {
-  width = parseInt(d3.select("#chart").style("width")) - margin.left - margin.right,
-  height = parseInt(d3.select("#chart").style("height")) - margin.top - margin.bottom;
-
-  treemap.size([width, height]);
-  treemap(root);
-
-  var leaves = svg.selectAll("g g");
-  leaves.data(root.leaves())
-    .transition()
-      .duration(300)
-      .ease(d3.easeQuadInOut)
-      .attr("transform", function(d) { return "translate(" + d.x0 + "," + d.y0 + ")"; });
-  leaves.exit().remove();
-
-  leaves.selectAll("rect")
-    .transition()
-      .duration(300)
-      .ease(d3.easeQuadInOut)
-      .attr("width", function(d) { return d.x1 - d.x0; })
-      .attr("height", function(d) { return d.y1 - d.y0; });
-  leaves.selectAll("rect").exit().remove();
-
+function updateLabels() {
   var backG = svg.select("g.background");
   var titleG = svg.select("g.titles");
+
   backG.selectAll("text").remove();
   titleG.selectAll("text").remove();
 
-  if (width < 350) {
+  if (width <= 300) {
     return;
   }
+
   root.children.forEach(function(c1) {
     c1.children.forEach(function(c2) {
       // For larger segments, include for both men and women
@@ -114,55 +75,131 @@ function resize() {
 }
 
 
-d3.csv(baseUrl + '/data/treemap_ssl_groups.csv', function(error, data) {
-  csvData = data;
-  treemapForScore();
+function updateData() {
+  var data = csvData;
+  var scoreData = data.filter(function(d) { return (d.ssl_score >= scoreNum) && (d.sex != "Other"); });
+  var scoreNest = d3.nest()
+    .key(function(d) { return d.race; })
+    .key(function(d) { return d.sex; })
+    .key(function(d) { return d.age; })
+    .rollup(function(d) { return d3.sum(d, function(d) { return +d.count; }); });
 
-  var cell = svg.selectAll("g")
-    .data(root.leaves())
-    .enter().append("g")
+  root = d3.hierarchy({values: scoreNest.entries(scoreData)}, function(d) { return d.values; })
+    .eachBefore(function(d) {
+      if (d.data.key) {
+        d.data.id = (d.parent ? d.parent.data.id + "." : "") + d.data.key.toLowerCase();
+      }
+      else {
+        d.data.id = "ssl";
+      }
+    })
+    .sum(function(d) { return d.value; })
+    .sort(function(a, b) { return b.height - a.height || b.value - a.value;; });
+
+  updateLayout();
+}
+
+
+function updateLayout() {
+  width = parseInt(d3.select("#chart").style("width")) - margin.left - margin.right,
+  height = parseInt(d3.select("#chart").style("height")) - margin.top - margin.bottom;
+
+  treemap.size([width, height]);
+  treemap(root);
+
+  var leafG = svg.selectAll("g.leafgroup")
+  var leaves = leafG.selectAll("g.leaves").data(root.leaves());
+  leaves.exit().remove();
+
+  // UPDATE SELECTION FOR CHANGED INFO
+
+  var leavesTransition = leaves.transition()
+      .duration(300)
+      .ease(d3.easeQuadInOut)
+      .attr("transform", function(d) { return "translate(" + d.x0 + "," + d.y0 + ")"; })
+    .select("rect")
+      .attr("width", function(d) { return d.x1 - d.x0; })
+      .attr("height", function(d) { return d.y1 - d.y0; });
+
+  // RE-ADD INFORMATION FOR NEW DATA
+
+  var leavesEnter = leaves.enter()
+    .append("g")
+      .attr("class", "leaves")
       .attr("transform", function(d) { return "translate(" + d.x0 + "," + d.y0 + ")"; });
 
-  cell.append("rect")
+  var rectLeaves = leavesEnter.append("rect")
+    .attr("id", function(d) { return d.data.id; })
+    .attr("class", function(d) { return d.data.id.split(".").join(" "); })
+    .attr("fill", function(d) { return color(d.parent.data.id); })
+    .attr("width", function(d) { return d.x1 - d.x0; })
+    .attr("height", function(d) { return d.y1 - d.y0; });
+
+  leavesEnter.append("clipPath")
+      .attr("id", function(d) { return "clip-" + d.data.id; })
+    .append("use")
+      .attr("xlink:href", function(d) { return "#" + d.data.id; });
+
+  leavesEnter.append("text")
+    .attr("clip-path", function(d) { return "url(#clip-" + d.data.id + ")"; })
+    .attr("x", 4)
+    .attr("y", 14)
+    .text(function(d) { return d.data.key; });
+
+  leavesEnter.append("title")
+    .text(function(d) { return d.data.key + "\n" + format(d.data.value); });
+  updateLabels();
+}
+
+
+d3.csv(baseUrl + '/data/treemap_ssl_groups.csv', function(error, data) {
+  csvData = data;
+  updateData();
+
+  width = parseInt(d3.select("#chart").style("width")) - margin.left - margin.right,
+  height = parseInt(d3.select("#chart").style("height")) - margin.top - margin.bottom;
+
+  treemap.size([width, height]);
+  treemap(root);
+
+  var leafG = svg.append("g").attr("class", "leafgroup")
+  svg.append("g").attr("class", "background");
+  svg.append("g").attr("class", "titles");
+
+  var leaves = leafG.selectAll("g.leaves")
+    .data(root.leaves())
+    .enter()
+    .append("g")
+      .attr("class", "leaves")
+      .attr("transform", function(d) { return "translate(" + d.x0 + "," + d.y0 + ")"; });
+
+  leaves.append("rect")
     .attr("id", function(d) { return d.data.id; })
     .attr("class", function(d) { return d.data.id.split(".").join(" "); })
     .attr("width", function(d) { return d.x1 - d.x0; })
     .attr("height", function(d) { return d.y1 - d.y0; })
     .attr("fill", function(d) { return color(d.parent.data.id); });
 
-  cell.append("clipPath")
+  leaves.append("clipPath")
       .attr("id", function(d) { return "clip-" + d.data.id; })
     .append("use")
       .attr("xlink:href", function(d) { return "#" + d.data.id; });
 
-  cell.append("text")
+  leaves.append("text")
     .attr("clip-path", function(d) { return "url(#clip-" + d.data.id + ")"; })
     .attr("x", 4)
     .attr("y", 14)
     .text(function(d) { return d.data.key; });
 
-  cell.append("title")
+  leaves.append("title")
     .text(function(d) { return d.data.key + "\n" + format(d.data.value); });
 
-  var backG = svg.append("g").attr("class", "background");
-  var titleG = svg.append("g").attr("class", "titles");
-
-  resize();
-  d3.select(window).on("resize", resize);
+  updateLabels();
+  d3.select(window).on("resize", updateData);
   d3.select("#scoreInput").on("input", function () {
     var el = document.getElementById("scoreInput");
     scoreNum = +el.value;
-    treemapForScore();
-    resize();
+    d3.select("#scoreVal").text(scoreNum);
+    updateData();
   });
-});
-
-
-d3.json(baseUrl + "/data/ssl_treemap_data.json", function(error, data) {
-  if (error) throw error;
-
-  var root = d3.hierarchy(data)
-    .eachBefore(function(d) { d.data.id = (d.parent ? d.parent.data.id + "." : "") + d.data.name; })
-    .sum(function(d) { return d.size; })
-    .sort(function(a, b) { return b.height - a.height || b.value - a.value; });
 });
